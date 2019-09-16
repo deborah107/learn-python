5ad4b790c93ca91fdae6aff2651c638271ee4500
keke <aneke.deborah@gmail.com>
2019-09-16 21:13:07 +0700

    passed exercise 3

--- 1-file-processing/3-merge-condition/merge.py
+++ 1-file-processing/3-merge-condition/merge.py
@@ -1,3 +1,19 @@
 def merge():
     # Merge all lines for all SQL files in the 'sqls' folder that include these keywords: INSERT, UPDATE, DELETE
-    print()
+    #print()
+
+    keywords = ["UPDATE", "INSERT", "DELETE"]
+
+    from os import listdir
+    from os.path import isfile, join
+    files = [f for f in listdir("sqls") if isfile(join("sqls", f))]
+    print(files)
+
+    with open('merged.sql', 'w') as outfile:
+        for content in files:
+            with open(join("sqls", content)) as files1:
+                for line in files1:
+                    for keyword in keywords:
+                        print(keywords, line)
+                        if keyword in line:
+                            outfile.write(line)
---
+++ 1-file-processing/3-merge-condition/merged.sql
@@ -0,0 +1,4 @@
+INSERT INTO users VALUES(1, 'wildan', 'syahidillah', '27-11-1995');
+UPDATE users SET first_name = 'charlie' WHERE first_name = 'wildan';
+INSERT INTO users VALUES(2, "alexander", "syahidillah", "27-11-1995");
+DELETE FROM users WHERE first_name = 'charlie';
